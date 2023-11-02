2612f474c37c297640544d485a94e5313883991c
0xNuru <nurudeenmyself@yahoo.com>
2023-11-02 16:56:24 +0100

    add 2-args mod4

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import sys
+    from sys import argv
 
-    argc = len(sys.argv) - 1
+    argc = len(argv) - 1
 
     if argc == 0:
-        print("{} arguments".format(argc))
+        print("{} arguments.".format(argc))
     elif argc == 1:
         print("{} argument:".format(argc))
     else:
         print("{} arguments:".format(argc))
 
     if argc >= 1:
-        i = 0
-        for arg in sys.argv:
-            if i != 0:
-                print("{}: {}".format(i, sys.argv[i]))
-            i += 1
+        argc = 0
+        for arg in argv:
+            if argc != 0:
+                print("{}: {}".format(argc, arg))
+            argc += 1
