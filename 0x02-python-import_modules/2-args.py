29ea3986d855a7d14c8887866c3fc6a15ceeffa7
0xNuru <nurudeenmyself@yahoo.com>
2023-11-02 16:29:06 +0100

    add 2-args mod2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,11 +6,12 @@ if __name__ == "__main__":
     argc = len(sys.argv) - 1
 
     if argc == 0:
-        print("0 arguments")
+        print("{} arguments".format(argc))
     elif argc == 1:
-        print("1 argument:")
+        print("{} argument:".format(argc))
     else:
         print("{} arguments:".format(argc))
-
-    for i in range(argc):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    
+    if argc >= 1:
+        for i in range(1, argc + 1):
+            print("{}: {}".format(i, sys.argv[i]))
