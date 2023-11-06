fc30cb79214e57007b1bff034dd26ecd28b8e9f3
0xNuru <nurudeenmyself@yahoo.com>
2023-11-06 11:36:54 +0100

    add 6-mat with tenary op

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,5 @@ def print_matrix_integer(matrix=[[]]):
     if matrix:  
         for l in matrix:
             for element in l:
-                print("{:d}".format(element), end=" ")
+                print("{:d}".format(element), end=" " if element != l[-1] else "")
             print()
