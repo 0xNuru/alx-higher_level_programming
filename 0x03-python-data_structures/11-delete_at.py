1e8428b66b1b02b020535175815791cf7c20280c
0xNuru <nurudeenmyself@yahoo.com>
2023-11-07 18:41:07 +0100

    add 11-del mod1

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -5,6 +5,6 @@ def delete_at(my_list=[], idx=0):
         if idx < 1 or idx > len(my_list):
             return my_list
 
-        result = [my_list[i] for i in range(len(my_list)) if i != idx]
-        
-        return result
+        del my_list[idx]
+
+        return my_list
