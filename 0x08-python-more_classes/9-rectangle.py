cf50180087a4384f1b0dc89dc9ddba6ab10e9c12
0xNuru <nurudeenmyself@yahoo.com>
2023-11-27 11:58:59 +0100

    add 9-rect mod

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -110,6 +110,6 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
-        """A function that returns a new Rectangle 
+        """A function that returns a new Rectangle
         instance with width == height == size"""
         return cls(size, size)
