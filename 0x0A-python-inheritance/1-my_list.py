2a91d8fa18478af00ff6a0f493e1b9e1c9dbd93c
0xNuru <nurudeenmyself@yahoo.com>
2023-12-04 14:39:12 +0100

    add 1-sorted_list mod

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,6 +3,8 @@
 
 
 class MyList(list):
+    """A class that defines a list based on the builtin list class"""
     def print_sorted(self):
+        """A method that prints a sorted version of a list"""
         sorted_list = sorted(self)
         print(sorted_list)
