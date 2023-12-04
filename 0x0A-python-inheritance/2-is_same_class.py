baf4d72ca3ac6b7c558805fd0db179b4ce0dbcd4
0xNuru <nurudeenmyself@yahoo.com>
2023-12-04 14:56:10 +0100

    add 2-issame mod1

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,5 +4,15 @@ instance of a specified class"""
 
 
 def is_same_class(obj, a_class):
-    """A function to check isinstance"""
+    """
+    Desc:
+        check if an obj is exactly an instance of a class
+
+    Args:
+        obj (any): Object
+        a_class (type): the class to be matched
+
+    Returns:
+        bool: True if match, otherwise false
+    """
     return type(obj) is a_class
