1860027aff604b1befb7c70ab7852350afccb34a
0xNuru <nurudeenmyself@yahoo.com>
2023-12-04 16:08:03 +0100

    add 8-base docstringmod

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -4,7 +4,9 @@ BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    """A cls that defines the rect instance"""
     def __init__(self, width, height):
+        """A method to construct the rect"""
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
