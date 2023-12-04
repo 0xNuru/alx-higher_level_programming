1ba6db2e221bd91e98572f1b166f7b0ffd867cb6
0xNuru <nurudeenmyself@yahoo.com>
2023-12-04 16:05:47 +0100

    add 8-base

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module contains a Rectangle class that inherits from BaseGeo"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
