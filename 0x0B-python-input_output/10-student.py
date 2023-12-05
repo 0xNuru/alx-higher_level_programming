2d52c5e5cf196cf6e9771d89a69149093ff5a6b2
0xNuru <nurudeenmyself@yahoo.com>
2023-12-06 00:41:22 +0100

    add 10#!

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -19,7 +19,7 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """
         Desc:
             JSONify object
