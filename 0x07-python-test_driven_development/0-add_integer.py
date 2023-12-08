7c4924da39dd70429ecb410f731ebe4288f6436f
0xNuru <nurudeenmyself@yahoo.com>
2023-12-08 10:35:16 +0100

    add TDD's 0task

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This module contains a function to add two ints"""
+
+
+def add_integer(a, b=98):
+    """
+    Adds two integers and returns the sum.
+
+    Args:
+        a: The first integer.
+        b: The second integer (default: 98).
+
+    Raises:
+        TypeError: If either `a` or `b` is not an integer.
+
+    Returns:
+        The sum of `a` and `b`.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+
+    return a + b
