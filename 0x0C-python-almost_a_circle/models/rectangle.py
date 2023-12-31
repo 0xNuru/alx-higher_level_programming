231ee27a2836d708ba2feb08c6d028467952dcd7
0xNuru <nurudeenmyself@yahoo.com>
2023-12-31 12:39:39 +0100

    add more rectangle setter validation mod1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -9,10 +9,10 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initializatin of variables"""
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
     @property
@@ -21,7 +21,7 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        if not type(value) == int:
+        if type(value) != int:
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
@@ -33,7 +33,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        if not type(value) == int:
+        if type(value) != int:
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
@@ -45,7 +45,7 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        if not type(value) == int:
+        if type(value) != int:
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
@@ -57,7 +57,7 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
-        if not type(value) == int:
+        if type(value) != int:
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
