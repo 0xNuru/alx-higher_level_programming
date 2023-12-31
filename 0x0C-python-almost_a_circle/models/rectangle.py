f0cc7f872e6b6ac8c5848132561291770988b265
0xNuru <nurudeenmyself@yahoo.com>
2023-12-31 09:58:05 +0100

    add rectangle class import fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module contains Rectangle class"""
 
-from base import Base
+from models.base import Base
 
 
 class Rectangle(Base):
@@ -37,7 +37,7 @@ class Rectangle(Base):
     def x(self):
         """getter for x"""
         return self.__x
-    @wx.setter
+    @x.setter
     def x(self, value):
         """setter for x"""
         self.__x = value
