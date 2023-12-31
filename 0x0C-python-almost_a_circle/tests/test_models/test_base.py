7b4f5a9a4a885263c0a7612333e5344a019c5fa4
0xNuru <nurudeenmyself@yahoo.com>
2023-12-31 08:43:31 +0100

    add base.py, test_base.py

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""Unit test for the base class"""
+
+import unittest
+from models.base import Base
+
+
+
+class TestBaseClass(unittest.TestCase):
+    """Test the Base class"""
+    def setUp(self):
+        Base._Base__nb_objects = 0
+
+    def test_nb_objects_attr(self):
+        """Test the private class attribute __nb_objects"""
+        self.assertIsNotNone(Base._Base__nb_objects)
+        self.assertEqual(Base._Base__nb_objects, 0)
+        Base()
+        Base()
+        Base()
+        self.assertEqual(Base._Base__nb_objects, 3)
+    
+    def test_default_id(self):
+        """Test the instantiation of a base object with no given id"""
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+
+    def test_id(self):
+        """Test when id is provided"""
+        b1 = Base()
+        b2 = Base(99)
+        b3 = Base()
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 99)
+        self.assertEqual(b3.id, 2)
+
+    def test_init_with_zero_id(self):
+        b1 = Base(0)
+        self.assertEqual(b1.id, 0)
+
+    def test_init_with_negative_id(self):
+        b1 = Base(-5)
+        self.assertEqual(b1.id, -5)
+
+    def test_init_with_large_id(self):
+        b1 = Base(10**6)
+        self.assertEqual(b1.id, 10**6)
+
+    def test_kwargs(self):
+        b1 = Base(id=99)
+        self.assertEqual(b1.id, 99)
+
+
+if __name__ == "__main__":
+    unittest.main()
