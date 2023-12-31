35ce83bea2f19d734120a29853bb361b9be912c4
0xNuru <nurudeenmyself@yahoo.com>
2023-12-31 09:53:02 +0100

    add rectangle class doc mod

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,6 +8,7 @@ class Rectangle(Base):
     """This class defines rectangle objects"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Initializatin of variables"""
         self.__width = width
         self.__height = height
         self.__x = x
@@ -16,29 +17,37 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """getter for width"""
         return self.__width
     @width.setter
     def width(self, value):
+        """setter for width"""
         self.__width = value
 
     @property
     def height(self):
+        """getter for height"""
         return self.__height
     @height.setter
     def height(self, value):
+        """setter for height"""
         self.__height = value
 
     @property
     def x(self):
+        """getter for x"""
         return self.__x
     @wx.setter
     def x(self, value):
+        """setter for x"""
         self.__x = value
 
     @property
     def y(self):
+        """getter for y"""
         return self.__y
     @y.setter
     def y(self, value):
+        """setter for y"""
         self.__y = value
 
