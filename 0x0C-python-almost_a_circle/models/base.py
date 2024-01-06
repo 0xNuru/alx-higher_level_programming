589844ab1ef862b52b38213c6b6fc67fb48a5900
0xNuru <nurudeenmyself@yahoo.com>
2024-01-06 20:10:47 +0100

    add square dict_tojson

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module contains a Base class"""
-
+import json
 
 class Base:
     """This is a Base class"""
@@ -13,3 +13,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """the JSON string repr of a list of dicts"""
+        if list_dictionaries is None  or list_dictionaries == []:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
