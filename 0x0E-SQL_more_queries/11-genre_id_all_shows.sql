e398d9153cd890182bf3a069417f287cb7bd320e
0xNuru <nurudeenmyself@yahoo.com>
2024-01-17 23:22:04 +0100

    add task 11 mod

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,1 +1,7 @@
+-- This script lists all shows contained in the database hbtn_0d_tvshows.
 
+SELECT shows.title, genres.genre_id
+FROM tv_shows AS shows
+LEFT JOIN tv_show_genres AS genres
+ON genres.show_id = shows.id
+ORDER BY shows.title, genres.genre_id;
