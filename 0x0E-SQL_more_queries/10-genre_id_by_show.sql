c5c5b1df0cf3de0050a4a22c727701d01ca23b8c
0xNuru <nurudeenmyself@yahoo.com>
2024-01-17 23:10:03 +0100

    add task 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,1 +1,7 @@
+-- This script lists all shows in hbtn_0d_tvshows that have genre
 
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_show_genres as genres
+JOIN tv_shows as shows
+ON genres.show_id = shows.id
+ORDER BY shows.title, genres.genre_id;
