3095116341af7759d95ef670b81392df7b5ac5fb
0xNuru <nurudeenmyself@yahoo.com>
2024-01-17 23:31:53 +0100

    add task 12

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,7 @@
 -- This script lists all shows in hbtn_0d_tvshows that have genre
 
-SELECT shows.title, genres.genre_id
-FROM tv_shows as shows
-JOIN tv_show_genres as genres
-ON genres.show_id = shows.id
-ORDER BY shows.title, genres.genre_id;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_show_genres
+JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,7 +1,7 @@
 -- This script lists all shows contained in the database hbtn_0d_tvshows.
 
-SELECT shows.title, genres.genre_id
-FROM tv_shows AS shows
-LEFT JOIN tv_show_genres AS genres
-ON genres.show_id = shows.id
-ORDER BY shows.title, genres.genre_id;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
