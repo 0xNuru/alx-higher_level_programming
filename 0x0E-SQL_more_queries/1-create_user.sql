bc0389c2d41e620fc746602356c818b07d5564f7
0xNuru <nurudeenmyself@yahoo.com>
2024-01-17 18:20:36 +0100

    add task 1mod

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -5,3 +5,7 @@ CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- best practice is to FLUSH the PREVILEGES table albeit sometimes redundant
 FLUSH PRIVILEGES;
 
+-- grant all previleges on all databases and all tables
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
+
