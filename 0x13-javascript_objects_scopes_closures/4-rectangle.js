3901e5db0a3820c813e57cae7e627a81bf724e16
0xNuru <nurudeenmyself@yahoo.com>
2024-02-14 00:55:36 +0100

    add semistandard linting

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,31 +1,29 @@
 #!/usr/bin/node
 
 class Rectangle {
-	constructor(w, h) {
-		if (typeof w === 'number' && w > 0) {
-			if (typeof h === 'number' && h > 0) {
-				this.width = w;
-				this.height = h;
-			}
-		}
-	}
+  constructor (w, h) {
+    if (typeof w === 'number' && w > 0) {
+      if (typeof h === 'number' && h > 0) {
+        this.width = w;
+        this.height = h;
+      }
+    }
+  }
 
-	print() {
-		for (let i = 0; i < this.height; i++)
-			console.log('X'.repeat(this.width));
-	}
-	
-	rotate() {
-		let temp = this.width;
-		this.width = this.height;
-		this.height = temp;
-	}
+  print () {
+    for (let i = 0; i < this.height; i++) { console.log('X'.repeat(this.width)); }
+  }
 
-	double() {
-		this.width *= 2;
-		this.height *= 2;
-	}
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
 
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
 }
 
 module.exports = Rectangle;
