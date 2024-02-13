055069426b6a70def18669487211bced94b1456e
0xNuru <nurudeenmyself@yahoo.com>
2024-02-13 01:41:21 +0100

    add task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
 
-let x = 0;
-
-if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
-  x = Number(process.argv[2]);
-}
-
-for (let i = 0; i < x; i++) {
-	for (let j = 0; j < x; j++)
-		console.log('X');
+  const x = Number(process.argv[2]);
+  let i = 0;
+  while (i < x) {
+    console.log('X'.repeat(x));
+    i++;
+  }
 }
