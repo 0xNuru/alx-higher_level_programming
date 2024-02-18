7be0edcba4e843067a46f4496ef57d1024ebc740
0xNuru <nurudeenmyself@yahoo.com>
2024-02-18 13:46:35 +0100

    mod task 1 and make it case sensitive

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""a script that lists all states with a name starting with N from db"""
+"""
+ a script that takes in an argument and displays all values in the states table
+ of hbtn_0e_0_usa where name matches the argument.
+"""
 
 import MySQLdb
 from sys import argv
@@ -8,7 +11,7 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3], charset="utf8")
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states WHERE name like 'N%' ORDER BY id ASC")
     rows = cursor.fetchall()
 
     for row in rows:
