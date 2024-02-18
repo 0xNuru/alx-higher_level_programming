48c3230cf7fbf94c582219036dfe7748473188ac
0xNuru <nurudeenmyself@yahoo.com>
2024-02-18 15:24:04 +0100

    add task 5 pcs mod

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-a script that takes in the name of a state as an argument and 
+a script that takes in the name of a state as an argument and
 lists all cities of that state, using the database hbtn_0e_4_usa
 """
 
@@ -12,8 +12,8 @@ if __name__ == "__main__":
                            passwd=argv[2], db=argv[3], charset="utf8")
     cursor = conn.cursor()
     cursor.execute("SELECT cities.name FROM cities \
-                    JOIN states ON cities.state_id = states.id WHERE states.name LIKE %s \
-                    ORDER BY cities.id", (argv[4],))
+                    JOIN states ON cities.state_id = states.id WHERE \
+                    states.name LIKE %s ORDER BY cities.id", (argv[4],))
     rows = cursor.fetchall()
     print(", ".join(city[0] for city in rows))
     cursor.close()
