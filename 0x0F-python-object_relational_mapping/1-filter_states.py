b5209c43ed17a1825d365eb42659198e7320b613
0xNuru <nurudeenmyself@yahoo.com>
2024-02-18 13:52:57 +0100

    mod task 1 and make it case sensitive

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3], charset="utf8")
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY \
+                    'N%' ORDER BY id ASC")
     rows = cursor.fetchall()
 
     for row in rows:
