47c5da2ca0e0e3d04f15f107a276d3ae94ee6f56
0xNuru <nurudeenmyself@yahoo.com>
2024-03-27 07:12:29 +0100

    add script to read input file and semistandard lint

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 // a script that reads and prints the content of a file
-const fs = require('fs')
+const fs = require('fs');
 
-const file = process.argv[2]
+const file = process.argv[2];
 fs.readFile(file, 'utf8', (err, data) => {
-	if (err) {
-		console.error(err)
-		return
-	}
-	console.log(data)
-})
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
